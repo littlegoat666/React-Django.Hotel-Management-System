154b4d19029d7cd62dede6c7910fd3fbeb19e610
LittleGoat <jiiirayu2546@gmail.com>
2025-06-11 11:39:07 +0700

    -

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -81,8 +81,8 @@ export const cities = [
     "London",
 ];
 
-// Exclusive Offers Dummy Data
-export const exclusiveOffers = [
+// SpecialOffers Offers Dummy Data
+export const specialOffers = [
     { _id: 1, title: "Summer Escape Package", description: "Enjoy a complimentary night and daily breakfast", priceOff: 25, expiryDate: "Aug 31", image: exclusiveOfferCardImg1 },
     { _id: 2, title: "Romantic Getaway", description: "Special couples package including spa treatment", priceOff: 20, expiryDate: "Sep 20", image: exclusiveOfferCardImg2 },
     { _id: 3, title: "Luxury Retreat", description: "Book 60 days in advance and save on your stay at any of our luxury properties worldwide.", priceOff: 30, expiryDate: "Sep 25", image: exclusiveOfferCardImg3 },
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import FeaturedDestination from '../components/FeaturedDestination'
+import SpecialOffers from '../components/SpecialOffers'
+import Testimonial from '../components/Testimonial'
+import NewLetter from '../components/NewLetter'
 
 const Home = () => {
   return (
     <>
         <Hero />
+        <FeaturedDestination />
+        <SpecialOffers />
+        <Testimonial />
+        <NewLetter />
     </>
   )
 }
